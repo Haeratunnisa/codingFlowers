6e39717fc741cc05421df7fd6fbbf484e08e1e07
Haeratunnisa <Hairatunnisa14@gmail.com>
2025-03-25 11:43:25 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ onload = () => {
       if (index < titles.length) {
         titleElement.innerHTML += titles[index];
         index++;
-        setTimeout(appendTitle, 100); // 1000ms delay
+        setTimeout(appendTitle, 700); // 1000ms delay
       }
     }
 
